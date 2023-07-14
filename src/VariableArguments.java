f0cac48e7c797575d808cbe6e129bc6afc10ed68
MurattErginn <muratergin0033@gmail.com>
2023-07-14 13:50:47 +0300

    Initial commit.

---
+++ src/VariableArguments.java
@@ -0,0 +1,18 @@
+/*
+    This class is a demo for Variable Arguments
+ */
+public class VariableArguments {
+
+    public static void main(String[] args) {
+        int sum = sum(1,2,3,4,5);
+        System.out.println(sum);
+    }
+
+    public static int sum(int ... nums) { // int ... means user can enter as much as int variable. (Variable Arguments)
+        int sum = 0;
+        for (int num:nums) {
+            sum += num;
+        }
+        return sum;
+    }
+}
